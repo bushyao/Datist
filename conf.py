4eacda3c54108af32140d34f89bae511d9be757b
bushyao <bushyao@163.com>
2018-01-07 21:18:20 +0800

    s
    s

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'数据专家文档'
-copyright = u'2017~2020, Datist'
+copyright = u'2009~2018, DSS'
 author = u'Bushyao'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'alabaster'
-html_theme = 'guzzle_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
